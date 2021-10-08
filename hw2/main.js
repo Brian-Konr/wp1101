e3724f3184f71c5a163ce4527248f10ee7f2f628
Brian-Konr <b08705038@ntu.edu.tw>
2021-10-08 17:04:57 +0800

    finish hw1 basic requiement

--- hw2/main.js
+++ hw2/main.js
@@ -5,18 +5,14 @@ const streetImgArr = [
     "https://images.unsplash.com/photo-1548930776-9680c40bbd95?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHN0cmVldHxlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
     "https://images.unsplash.com/photo-1544550285-f813152fb2fd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHN0cmVldHxlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
     "https://images.unsplash.com/photo-1620888836251-0204241bef7b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHN0cmVldHxlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    "https://images.unsplash.com/photo-1552819686-25b0c726045d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c3RyZWV0fGVufDB8MnwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    "https://images.unsplash.com/photo-1617564112851-8dc6d3f4ec34?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c3RyZWV0fGVufDB8MnwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    "https://images.unsplash.com/photo-1593940836716-38a8ee2c5eb4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c3RyZWV0fGVufDB8MnwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    "https://images.unsplash.com/photo-1548930776-9680c40bbd95?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHN0cmVldHxlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    "https://images.unsplash.com/photo-1544550285-f813152fb2fd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHN0cmVldHxlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    "https://images.unsplash.com/photo-1620888836251-0204241bef7b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHN0cmVldHxlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
 ];
 const foodAndDrinkArr = [
     "https://images.unsplash.com/photo-1422748733255-ee572fddeab0?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGZvb2QlMjAlMjYlMjBkcmlua3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
     "https://images.unsplash.com/photo-1556804335-2fa563e93aae?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Zm9vZCUyMCUyNiUyMGRyaW5rfGVufDB8MnwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
     "https://images.unsplash.com/photo-1535140728325-a4d3707eee61?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGZvb2QlMjAlMjYlMjBkcmlua3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    "https://images.unsplash.com/photo-1502898664531-0564045a0da9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGZvb2QlMjAlMjYlMjBkcmlua3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    "https://images.unsplash.com/photo-1502898664531-0564045a0da9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGZvb2QlMjAlMjYlMjBkcmlua3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1591735200387-c29c7d0ec3d3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fGZvb2QlMjAlMjYlMjBkcmlua3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1625827626291-6fbd47a431ae?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fGZvb2QlMjAlMjYlMjBkcmlua3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
 ]
 const natureArr = [
     "https://images.unsplash.com/photo-1566704351700-da9069468d94?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8MnwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
@@ -24,9 +20,10 @@ const natureArr = [
     "https://images.unsplash.com/photo-1471180625745-944903837c22?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bmF0dXJlfGVufDB8MnwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
     "https://images.unsplash.com/photo-1570030990547-f6b13f3062ff?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bmF0dXJlfGVufDB8MnwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
     "https://images.unsplash.com/photo-1465188035480-cf3a60801ea5?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bmF0dXJlfGVufDB8MnwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1606318621597-c057f7d4926e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fG5hdHVyZXxlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
 ]
 let currentIndex = 0;
-let currentArr = [];
+let currentArr = streetImgArr;
 
 let displayBoxImg = document.getElementById("displayBoxImg"),
     prevButton = document.getElementById("prevButton"),
@@ -102,4 +99,8 @@ function switchAlbum(album) {
     if(album === 'street') renderPic(streetImgArr);
     else if(album === 'food') renderPic(foodAndDrinkArr);
     else if(album === 'nature') renderPic(natureArr);
+    else {
+        alert("此相簿為空！")
+        renderPic(currentArr);
+    }
 }
