aaa1f0df88120b7a9409f7c9ec63796b2e1bfd44
Brian-Konr <b08705038@ntu.edu.tw>
2021-10-15 22:49:18 +0800

    update

--- hw3/main.js
+++ hw3/main.js
@@ -1,4 +1,5 @@
-var itemCount = 0;
+var itemCount = 0,
+    completeCount = 0;
 
 class TODO_ITEM {
     constructor(item_detail) {
@@ -11,6 +12,19 @@ class TODO_ITEM {
         var input = document.createElement("input");
         input.type = "checkbox";
         input.id = itemCount;
+        input.addEventListener('change', function() {
+            var index = Number(this.id);
+            let text = document.getElementsByClassName("todo-app__item-detail")[index];
+            if(this.checked) {
+                text.classList.add("complete");
+                completeCount++;
+            }
+            else if(!this.checked) {
+                text.classList.remove("complete");
+                completeCount--;
+            }
+            setUnCompleted();
+        })
         
         var label = document.createElement("label");
         label.htmlFor = input.id;
@@ -28,15 +42,27 @@ class TODO_ITEM {
     get getItem() {return this.item;}
 }
 
-let inputField = document.getElementById("todo-input");
-inputField.addEventListener('keypress', setItem);
+let footer = document.getElementById("todo-footer"),
+    inputField = document.getElementById("todo-input"),
+    list = document.getElementById("todo-list");
 
-function setItem(e) {
-    if(e.key === 'Enter') {
+
+inputField.addEventListener('keypress', handleEnter);
+
+//initialize
+footer.classList.add("hide");
+
+function handleEnter(e) {
+    if(e.key === 'Enter' && inputField.value !== "") {
         console.log(inputField.value);
-        let listNode = document.getElementById("todo-list");
-        listNode.appendChild(new TODO_ITEM(inputField.value).getItem);
+        list.appendChild(new TODO_ITEM(inputField.value).getItem);
         inputField.value = '';
         itemCount++;
+        footer.classList.remove("hide");
+        setUnCompleted();
     }
+}
+
+function setUnCompleted() {
+    footer.children[0].textContent = `${itemCount - completeCount} left`;
 }
