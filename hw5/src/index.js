3e2d0dcd5bc63baced26ea9c938278d25ad63dc7
Brian-Konr <b08705038@ntu.edu.tw>
2021-10-31 17:31:50 +0800

    initialize

--- hw5/src/index.js
+++ hw5/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import TodoApp from './TodoApp';
+import App from './App'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoApp />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
