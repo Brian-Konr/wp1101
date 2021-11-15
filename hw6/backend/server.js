22ee1143ff138cb4f257803df56d60b72ac66975
Brian-Konr <b08705038@ntu.edu.tw>
2021-11-16 00:42:02 +0800

    init hw6

---
+++ hw6/backend/server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+const server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+});
+
+server.listen(PORT, function() {
+    console.log(`Server listening on: http://localhost:${PORT}`);
+});
